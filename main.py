e811fe652307869b4d33811aa1ddb097a439a421
leen-b205 <leenb1414@gmail.com>
2026-09-13 16:53:55 +0300

    Create student profile

---
+++ main.py
@@ -0,0 +1,8 @@
+name = "Leen"
+major = "software engineering"
+university = "King Saud University"
+print("Student Profile")
+print("----------------")
+print("Name:", name)
+print("Major:", major)
+print("University:", university)
