58e37de21562b10c127a1c89dfbf19bbb7cd2159
leen-b205 <leenb1414@gmail.com>
2026-09-13 16:55:30 +0300

    Add more student information

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 name = "Leen"
 major = "software engineering"
 university = "King Saud University"
+graduation_year = "2030"
 print("Student Profile")
 print("----------------")
 print("Name:", name)
 print("Major:", major)
 print("University:", university)
+print("Graduation Year:", graduation_year)
